7a3e40e1e4e654d7b90c05df0d0b57caebd5fe77
beef-erikson <beef.erikson.studios@gmail.com>
2019-01-02 08:13:26 -0500

    end of day commit - shit be broken.

--- testing-grounds.cs
+++ testing-grounds.cs
@@ -1,26 +1,55 @@
 using System;
 using Gtk;
+using Gdk;
 
 public class Heyo
 {
+    // The tray Icon
+    private static StatusIcon trayIcon;
+
+    // The window
+    private static Gtk.Window window;
+
     public static void Main()
     {
+        // Initialize GTK#
         Application.Init();
 
         // Creates window
-        Window myWin = new Window("This is a window!");
-        myWin.Resize(600,600);
+        window = new Gtk.Window("This is a window!");
 
-        // Creates a label and puts some text in it
-        Label myLabel = new Label();
-        myLabel.Text = "Hi. Hope you're day / evening is well, fellow devs o7\nHappy New Year!!\n\nMore interesting things to come here after sleep!";
+        // Attach to the Delete Event when the window has been closed.
+        window.DeleteEvent += delegate { Application.Quit(); };
 
-        // Adds label to form
-        myWin.Add(myLabel);
+        // Create the icon
+        trayIcon = new StatusIcon(new Pixbuf("someIcon.png"));
+        trayIcon.Visible = true;
 
-        // Show everything
-        myWin.ShowAll();
+        // Show/Hide the window when the TrayIcon has been clicked.
+        trayIcon.Activate += delegate { window.Visible = !window.Visible; };
+        // Show a pop up menu when the icon has been right clicked.
+        trayIcon.PopupMenu += OnTrayIconPopup;
 
+        // A Tooltip for the Icon
+        trayIcon.Tooltip = "Yep. This is a tooltip.";
+
+        // Show main window and start the app
+        window.ShowAll();
         Application.Run();
+    }
+
+    // Create the popup menu, on right click
+    static void OnTrayIconPopup(object o, EventArgs args)
+    {
+        Menu popupMenu = new Menu();
+        ImageMenuItem menuItemQuit = new ImageMenuItem("Quit");
+        Gtk.Image appimg = new Gtk.Image(Stock.Quit, IconSize.Menu);
+        menuItemQuit.Image = appimg;
+        popupMenu.Add(menuItemQuit);
+
+        // Quit the application when quit has been clicked
+        menuItemQuit.Activated += delegate { Application.Quit(); };
+        popupMenu.ShowAll();
+        popupMenu.Popup();
     }
 }
